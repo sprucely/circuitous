3233749ca31b1cca9e7ba687e2193702726bbcfb
Spruce Weber-Milne <timberdig@gmail.com>
2016-06-11 12:38:10 -0700

    initial commit

---
+++ desktop/src/com/circuitous/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.circuitous.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.circuitous.game.CircuitousGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CircuitousGame(), config);
+	}
+}
